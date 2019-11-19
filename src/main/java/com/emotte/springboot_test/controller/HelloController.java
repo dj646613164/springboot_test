8dce1f7002f45e76464a536a38471aaa652bd077
dujie <dujie@95081.com>
2019-11-19 10:13:17 +0800

    集成swagger和lombok

--- src/main/java/com/emotte/springboot_test/controller/HelloController.java
+++ src/main/java/com/emotte/springboot_test/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.emotte.springboot_test.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,14 +17,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @create: 2019-10-31 14:05
  **/
 @Controller
+@Api(tags = {"打招呼的controller"})
 public class HelloController {
 
+    @ApiOperation(value = "说你好",httpMethod="GET")
     @RequestMapping("/hello")
     @ResponseBody
     public String hello(){
         return "hello";
     }
 
+    @ApiOperation(value = "跳转页面",httpMethod = "GET")
     @RequestMapping("/index")
     public String index(Model model){
         model.addAttribute("title", "测试");
